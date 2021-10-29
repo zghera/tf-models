f52e78eb8ab3ab284ecf5df6ec5ab40240ae57dd
Abhirakshak <37345811+Abhirakshak@users.noreply.github.com>
2021-10-29 14:48:17 -0400

    updated with shapenet annotations

--- official/vision/beta/projects/mesh_rcnn/data/shapenet_tfrecord_gen.py
+++ official/vision/beta/projects/mesh_rcnn/data/shapenet_tfrecord_gen.py
@@ -1,13 +1,15 @@
 from absl import app  # pylint:disable=unused-import
 from absl import flags
 import numpy as np
-
+import json
 import tensorflow as tf
+import os
 
 import multiprocessing as mp
 from official.vision.beta.data import tfrecord_lib
 from research.object_detection.utils import dataset_util
 from official.vision.beta.data.tfrecord_lib import convert_to_feature
+from create_pix3d_tf_record import parse_obj_file
 
 flags.DEFINE_multi_string('shapenet_dir', '', 'Directory containing ShapeNet.')
 flags.DEFINE_string('output_file_prefix', '/tmp/train', 'Path to output file')
@@ -18,65 +20,21 @@ FLAGS = flags.FLAGS
 logger = tf.get_logger()
 logger.setLevel(logging.INFO)
 
+
 def create_tf_example(image):
-    with tf.io.gfile.GFile(os.join(image["shapenet_dir"], image["img"]), 'rb') as fid:
-        encoded_img_jpg = fid.read()
 
-    img_width, img_height = image["img_size"]
-    img_filename = image["img"]
-    img_category = image["category"]
-    keypoints_2d = image["2d_keypoints"]
+    model_id = image["model_id"]
+    label = image["label"]
 
-    feature_dict = {"img/height": convert_to_feature(img_height),
-                    "img/width": convert_to_feature(img_width),
-                    "img/category": convert_to_feature(img_category),
-                    "img/filename": convert_to_feature(img_filename),
-                    "img/encoded": convert_to_feature(encoded_img_jpg),
-                    "img/2d_keypoints": convert_to_feature(keypoints_2d)}
+    temp_file_dir = os.join(image["shapenet_dir"], image["synset_id"])
+    model_vertices, model_faces = parse_obj_file(os.join(temp_file_dir, image["model_id"]))
 
-    with tf.io.gfile.GFile(os.join(image["shapenet_dir"], image["mask"]), 'rb') as fid:
-        encoded_mask_jpg = fid.read()
-    
-    feature_dict.update({"mask": convert_to_feature(encoded_mask_jpg)})
+    feature_dict = {"model_id": convert_to_feature(model_id),
+                    "label": convert_to_feature(label),
+                    "vertices": convert_to_feature(model_vertices),
+                    "faces": convert_to_feature(model_faces)}
 
-    with tf.io.gfile.GFile(os.join(image["shapenet_dir"], image["model"]), 'rb') as fid:
-        encoded_model = fid.read()
 
-    with tf.io.gfile.GFile(os.join(image["shapenet_dir"], image["3d_keypoints"]), 'rb') as fid:
-        keypoints_3d = fid.read()
-
-    model_raw = image["model_raw"]
-    model_source = image["model_source"]
-
-    feature_dict.update({"model": convert_to_feature(encoded_model),
-                         "model/raw": convert_to_feature(model_raw),
-                         "model/source": convert_to_feature(model_source),
-                         "model/3d_keypoints": convert_to_feature(keypoints_3d)})
-
-    with tf.io.gfile.GFile(os.join(image["shapenet_dir"], image["voxel"]), 'rb') as fid:
-        encoded_voxel = fid.read()
-
-    rot_mat = image["rot_mat"]
-    trans_mat = image["trans_mat"]
-    focal_length = image["focal_length"]
-    cam_position = image["cam_positon"]
-    inplane_rotation = image["inplane_rotation"]
-    truncated = image["truncated"]
-    occluded = image["occluded"]
-    slightly_occluded = image["slightly_occluded"]
-    bbox = image["bbox"]
-
-    feature_dict.update({"voxel": convert_to_feature(encoded_voxel),
-                         "rot_mat": convert_to_feature(rot_mat),
-                         "trans_mat": convert_to_feature(trans_mat),
-                         "focal_length": convert_to_feature(focal_length),
-                         "cam_position": convert_to_feature(cam_position),
-                         "inplane_rotation": convert_to_feature(inplane_rotation),
-                         "truncated": convert_to_feature(truncated),
-                         "occluded": convert_to_feature(occluded),
-                         "slightly_occluded": convert_to_feature(slightly_occluded),
-                         "bbox": convert_to_feature(bbox)})
-    
     example = tf.train.Example(
         features=tf.train.Features(feature=feature_dict))
 
@@ -84,12 +42,10 @@ def create_tf_example(image):
 
 def generate_annotations(images, shapenet_dir):
     for image in images:
-        yield {"img": image["img"], "category": image["category"], "img_size": image["img_size"], "2d_keypoints": image["2d_keypoints"],
-               "mask": image["mask"], "img_source": image["img_source"], "model": image["model"], "model_raw": image["model_raw"],
-               "model_source": image["model_source"], "3d_keypoints": image["3d_keypoints"], "voxel": image["voxel"], "rot_mat": image["rot_mat"],
-               "trans_mat": image["trans_mat"], "focal_length": image["focal_length"], "cam_position": image["cam_position"],
-               "inplane_rotation": image["inplane_rotation"], "truncated": image["truncated"], "occluded": image["occluded"],
-               "slightly_ocluded": image["slightly_occluded"], "bbox": image["bbox"], "shapenet_dir": shapenet_dir}
+        yield {"shapenet_dir": shapenet_dir, 
+                "label": image["label"], 
+                "model_id": image["model_id"],
+                "synset_id": image["synset_id"]}
 
 def _create_tf_record_from_shapenet_dir(shapenet_dir,
                                      output_path,
@@ -103,7 +59,22 @@ def _create_tf_record_from_shapenet_dir(shapenet_dir,
 
     logging.info('writing to output path: %s', output_path)
 
-    images = json.load(open(os.join(shapenet_dir, "taxonomy.json")))
+    #create synset ID to label mapping dictionary
+    with open(shapenet_synset_dict.json, "r") as dict_file:
+        synset_dict = json.load(dict_file)
+
+    #images list
+    images = []
+
+    for _, synset_directories, _ in os.walk(shapenet_dir):
+        for synset_directory in synset_directories:
+            for _, object_directories, _ in os.walk(os.path.join(shapenet_dir, synset_directory)):
+                for object_directory in object_directories:
+                    image = {"model_id" : object_directory, 
+                            "label" : synset_dict[synset_directory],
+                            "shapenet_dir": shapenet_dir,
+                            "synset_id": synset_directory}
+                    images.append(image)
 
     shapenet_annotations_iter = generate_annotations(
         images=images, shapenet_dir=shapenet_dir)
