d15c1f4fdf6ab860c164d0dc2efe64e64d4cd24b
Abhirakshak <37345811+Abhirakshak@users.noreply.github.com>
2021-11-09 02:23:11 -0500

    Update shapenet_visualizer.py

--- official/vision/beta/projects/mesh_rcnn/data/shapenet_visualizer.py
+++ official/vision/beta/projects/mesh_rcnn/data/shapenet_visualizer.py
@@ -93,8 +93,8 @@ def visualize_tf_record(shapenet_dir,
         example = tf.train.Example()
         example.ParseFromString(raw_record.numpy())
         features = example.features.feature
-        vertices = tf.io.parse_tensor(features["model/vertices"].bytes_list.value[0], tf.float32).numpy().tolist()
-        faces = tf.io.parse_tensor(features["model/faces"].bytes_list.value[0], tf.int32).numpy().tolist()
+        vertices = tf.io.parse_tensor(features["vertices"].bytes_list.value[0], tf.float32).numpy().tolist()
+        faces = tf.io.parse_tensor(features["faces"].bytes_list.value[0], tf.int32).numpy().tolist()
         mask = tf.io.parse_tensor(features["mask"].bytes_list.value[0], tf.int32).numpy().tolist()
         image = tf.io.parse_tensor(features["img/encoded"].bytes_list.value[0], tf.int32).numpy().tolist()
 
