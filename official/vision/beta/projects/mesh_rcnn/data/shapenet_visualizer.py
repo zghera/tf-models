5fe4e326e9e2c56fe5174aecab60441035f72180
Ethan Suleman <70169323+esuleman@users.noreply.github.com>
2021-11-09 02:21:21 -0500

    Update shapenet_visualizer.py

--- official/vision/beta/projects/mesh_rcnn/data/shapenet_visualizer.py
+++ official/vision/beta/projects/mesh_rcnn/data/shapenet_visualizer.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-# Author: JEthan Suleman & Abhi Raja
+# Author: Ethan Suleman & Abhi Raja
 
 r"""Takes TFRecord containing ShapeNet dataset and visualizes it.
 Example usage:
