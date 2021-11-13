5a9ad1e9f7d1578ec9dc4f1e1f101c74d8da8ee2
lenliu3 <alliucaneat@gmail.com>
2021-11-13 15:10:57 -0500

    added weight loader dir

---
+++ official/vision/beta/projects/mesh_rcnn/utils/weight_utils/test_load_weights.py
@@ -0,0 +1,5 @@
+from official.vision.beta.projects.mesh_rcnn.configs import \
+    mesh_rcnn as mesh_rcnn_config
+from official.vision.beta.projects.mesh_rcnn.modeling import factory
+from official.vision.beta.projects.mesh_rcnn.modeling.heads import voxel_head
+
