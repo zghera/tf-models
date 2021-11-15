5050eab6e17cab257eaafc4015ddf08c1971aad5
Wenxin Jiang <wenxinjiang@davisjam-t-MacBookPro162-13-inch-2020-4TB-3-port.local>
2021-11-15 15:09:16 -0500

    yolox decoder update

--- official/vision/beta/projects/yolo/modeling/decoders/yolo_decoder.py
+++ official/vision/beta/projects/yolo/modeling/decoders/yolo_decoder.py
@@ -87,12 +87,12 @@ YOLO_MODELS = {
               embed_spp=True,
                 use_fpn=False,
                 max_level_process_len=None,
-                path_process_len=6),
+                path_process_len=5),
             fpn=dict(
                 embed_spp=True,
                 use_fpn=True,
                 max_level_process_len=None,
-                path_process_len=6),
+                path_process_len=5),
             ),
 }
 
