de26398ecaa97b4152c809996dee2af90e51490b
Vishnu Banna <banna3vishnu@gmail.com>
2021-12-05 17:31:35 -0500

    modle

--- official/vision/beta/projects/yolo/ops/box_ops.py
+++ official/vision/beta/projects/yolo/ops/box_ops.py
@@ -247,8 +247,8 @@ def _compute_v_manual_grad(gt_width, gt_height, pred_width, pred_height):
     arcterm = tf.math.atan(termb) - tf.math.atan(terma)
 
     dv = tf.expand_dims(dv, axis = -1)
-    dv_dw = dv * (8 / math.pi**2) * arcterm * pred_height
-    dv_dh = -dv * (8 / math.pi**2) * arcterm * pred_width
+    dv_dw = -dv * (8 / math.pi**2) * arcterm * pred_height
+    dv_dh = dv * (8 / math.pi**2) * arcterm * pred_width
     return 0, 0, dv_dw, dv_dh 
   return v, delta
 
