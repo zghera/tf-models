12b90217a4f76bb9cf1a62516832e236cbea1967
Wenxin Jiang <wenxinjiang@pal-nat186-73-185.itap.purdue.edu>
2021-12-06 14:33:56 -0500

    Update Convs in yolox_head

--- official/vision/beta/projects/yolo/modeling/heads/yolox_head.py
+++ official/vision/beta/projects/yolo/modeling/heads/yolox_head.py
@@ -41,7 +41,6 @@ class YOLOXHead(tf.keras.layers.Layer):
       width_scaling = 1.0,
       strides=[8, 16, 32],
       in_channels=[256, 512, 1024],
-      depthwise=False,
       prior_prob = 1e-2,
       **kwargs):
 
@@ -80,10 +79,26 @@ class YOLOXHead(tf.keras.layers.Layer):
     self._classes = classes
     self._boxes_per_level = boxes_per_level
     self._output_extras = output_extras
-
+    self._width_scaling = width_scaling
     self._smart_bias = smart_bias
     self._use_separable_conv = use_separable_conv
     self._prior_prob = prior_prob
+
+    self._stems = dict()
+
+    self._bias= -tf.math.log((1 - self._prior_prob) / self._prior_prob)
+
+    self._base_config = dict(
+        activation=activation,
+        norm_momentum=norm_momentum,
+        norm_epsilon=norm_epsilon,
+        kernel_initializer=kernel_initializer,
+        kernel_regularizer=kernel_regularizer,
+        bias_regularizer=bias_regularizer)
+
+    
+
+  def build(self, input_shape):
 
     self._cls_convs = dict()
     self._reg_convs = dict()
@@ -91,60 +106,63 @@ class YOLOXHead(tf.keras.layers.Layer):
     self._cls_preds = dict()
     self._reg_preds = dict()
     self._obj_preds = dict()
-    self._stems = dict()
 
+    self._cls_head = dict()
+    self._obj_head = dict()
+    self._reg_head = dict()
     
-    self._bias= -tf.math.log((1 - self._prior_prob) / self._prior_prob)
-
-    Conv = nn_blocks.DWConv if depthwise else nn_blocks.BaseConv 
     for k in self._key_list:
-      self._stems[k] = nn_blocks.BaseConv(
-          filters=int(256 * width_scaling),
+      self._stems[k] = nn_blocks.ConvBN(
+          filters=int(256 * self._width_scaling),
           kernel_size=(1,1),
           strides=(1,1),
-          padding='same', 
-          # use_bn = True,
-          activation=activation,
+          use_bn=True,
+          use_separable_conv=self._use_separable_conv,
+          **self._base_config,
       )
 
       self._cls_convs[k] = Sequential(
           [
-          Conv(
-            filters=int(256 * width_scaling),
+          nn_blocks.ConvBN(
+            filters=int(256 * self._width_scaling),
             kernel_size=(3,3),
             strides=(1,1),
-            # use_bn = True,
-            activation=activation,
+            use_bn=True,
+            use_separable_conv=self._use_separable_conv,
+            **self._base_config,
           ),
-          Conv(
-            filters=int(256 * width_scaling),
+          nn_blocks.ConvBN(
+            filters=int(256 * self._width_scaling),
             kernel_size=(3,3),
             strides=(1,1),
-            # use_bn = True,
-            activation=activation,
+            use_bn=True,
+            use_separable_conv=self._use_separable_conv,
+            **self._base_config,
           ),
           ]
         )
 
       self._reg_convs[k] = Sequential(
           [
-            Conv(
-              filters=int(256 * width_scaling),
+            nn_blocks.ConvBN(
+              filters=int(256 * self._width_scaling),
               kernel_size=(3,3),
               strides=(1,1),
-              # use_bn = True,
-              activation=activation,
+              use_bn=True,
+              use_separable_conv=self._use_separable_conv,
+              **self._base_config,
             ),
-            Conv(
-              filters=int(256 * width_scaling),
+            nn_blocks.ConvBN(
+              filters=int(256 * self._width_scaling),
               kernel_size=(3,3),
               strides=(1,1),
-              # use_bn = True,
-              activation=activation,
+              use_bn=True,
+              use_separable_conv=self._use_separable_conv,
+              **self._base_config,
             ),
           ]
         )
-      
+        
       self._cls_preds[k] = tf.keras.layers.Conv2D(
           filters=self._boxes_per_level * self._classes,
           kernel_size=(1,1),
@@ -164,11 +182,6 @@ class YOLOXHead(tf.keras.layers.Layer):
           strides=(1,1),
           padding='same',
           bias_initializer=tf.keras.initializers.constant(self._bias))
-
-  def build(self, input_shape):
-    self._cls_head = dict()
-    self._obj_head = dict()
-    self._reg_head = dict()
 
     for key in self._key_list:
       self._cls_head[key] = Sequential()
