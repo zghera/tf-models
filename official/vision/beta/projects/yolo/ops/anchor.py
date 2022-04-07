2873977eb6abd92c43041a6dd222ed6e542f6536
RomanczuG <romanczug@icloud.com>
2022-04-06 23:48:00 -0400

    test

--- official/vision/beta/projects/yolo/ops/anchor.py
+++ official/vision/beta/projects/yolo/ops/anchor.py
@@ -392,9 +392,17 @@ class YoloAnchorLabeler:
 
     # construct the index update grid
     reps = tf.reduce_sum(tf.cast(is_in_boxes_and_center, tf.int16), axis=-2)
-    indexes = tf.cast(tf.where(is_in_boxes_and_center), tf.int32)
-    y, x, t, a = tf.split(indexes, 4, axis=-1)
+    # indexes = tf.cast(tf.where(is_in_boxes_and_center), tf.int32)
+    
 
+    (
+        num_fg,
+        gt_matched_classes,
+        pred_ious_this_matching,
+        indexes,
+    ) = self.dynamic_k_matching(box_delta, classes, len(classes), is_in_index)
+
+    y, x, t, a = tf.split(indexes, 4, axis=-1)
     boxes = tf.gather_nd(box_list, t)
     classes = tf.cast(tf.gather_nd(class_list, t), boxes.dtype)
     reps = tf.gather_nd(reps, tf.concat([y, x], axis=-1))
@@ -404,7 +412,7 @@ class YoloAnchorLabeler:
 
     # return the samples and the indexes
     samples = tf.concat([boxes, conf, classes], axis=-1)
-    indexes = tf.concat([y, x, a], axis=-1)
+    # indexes = tf.concat([y, x, a], axis=-1)
     return indexes, samples, num_anchors
 
   def build_label_per_path(self, 
@@ -492,3 +500,93 @@ class YoloAnchorLabeler:
       indexes[key], updates[key], true_grids[key] = self.build_label_per_path(
         key, boxes, classes, width, height, iou_index = iou_index)
     return indexes, updates, true_grids 
+
+
+  @tf.function()
+  # def dynamic_k_matching(cost, pair_wise_ious, gt_classes, num_gt, fg_mask):
+  def dynamic_k_matching(pair_wise_ious, gt_classes, num_gt, fg_mask):
+    """Builds the labels for a single image, not functional in batch mode. 
+    
+    Args: 
+      cost: `Tensor` of shape [None, 4] indicating the object locations in 
+        an image. 
+      pair_wise_ious: `Tensor` of shape [None] indicating the each objects classes.
+      gt_classes: `int` for the images width. 
+      num_gt: `int` for the images height.
+      fg_mask: `int` for the maximum number of expanded boxes to allow. 
+
+    Returns: 
+      num_fg: `Tensor` of shape [None, 3] of indexes in the final grid where 
+        boxes are located.  
+      gt_matched_classes: `Tensor` of shape [None, 8] the value to place in the final grid.
+      pred_ious_this_matching: `Tensor` of [width/stride, height/stride, num_anchors, 1] holding 
+        a mask of where boxes are locates for confidence losses. 
+      matched_gt_inds: 
+    """    
+    matching_matrix = tf.zeros_like(cost, dtype=tf.uint8)
+    flattened_idx = []
+    ious_in_boxes_matrix = pair_wise_ious
+    n_candidate_k = min(10, ious_in_boxes_matrix.shape[1])
+    topk_ious, _ = tf.math.top_k(ious_in_boxes_matrix, k=n_candidate_k)
+    dynamic_ks = tf.math.maximum(tf.cast(tf.reduce_sum(topk_ious, axis=1), tf.int32), [1])
+    
+    for gt_idx in range(num_gt):
+        num_top = dynamic_ks[gt_idx]
+        _, pos_idx = tf.math.top_k(
+            -cost[gt_idx], k=num_top, sorted=False)
+        
+
+        indices = tf.TensorArray(tf.int32, size = 0, dynamic_size = True, clear_after_read = False)
+        updates = tf.TensorArray(tf.int32, size = 0, dynamic_size = True, clear_after_read = False)
+        for id in range(len(pos_idx)):
+          indices = indices.write(id, [gt_idx, pos_idx[id]])
+          updates = updates.write(id, 1)
+
+        matching_matrix = tf.tensor_scatter_nd_update(matching_matrix, indices.stack(), tf.cast( updates.stack(), dtype = tf.uint8 ))
+
+        indices.close()
+        updates.close()
+
+    del topk_ious, dynamic_ks, pos_idx
+
+    anchor_matching_gt = tf.math.reduce_sum(matching_matrix, 0)
+    
+
+    # if tf.math.reduce_sum(tf.cast((anchor_matching_gt > 1), tf.int32)) > 0:
+    #   mask = anchor_matching_gt > 1
+    #   cost_after_mask = tf.boolean_mask(cost, mask, axis=1)
+    #   cost_argmin = tf.math.argmin(cost_after_mask)
+    #   mask = tf.reshape(mask, [1, len(mask)])
+    #   num_repeat = tf.constant([matching_matrix.shape[0], 1], tf.int32)
+    #   tiled_mask = tf.tile(mask, num_repeat)
+    #   indices = tf.where(tiled_mask)
+
+    #   updates = tf.TensorArray(tf.int64, size = 0, dynamic_size = True, clear_after_read = False)
+    #   for id in range(len(indices)):
+    #     updates.stack()
+    #     updates = updates.write(id, 0)
+        
+    #   matching_matrix = tf.tensor_scatter_nd_update(matching_matrix, indices, tf.cast(updates.stack(), dtype = tf.uint8))
+
+    #   indices = tf.TensorArray(tf.int32, size = 0, dynamic_size= True, clear_after_read = False)
+    #   updates = tf.TensorArray(tf.int32, size = 0, dynamic_size= True, clear_after_read = False)
+    #   costargmin_ind = 0
+    #   for id in range(len(mask[0])):
+    #     if mask[0][id]:
+    #       cost_ind = cost_argmin[costargmin_ind]
+    #       index = [cost_ind, id]
+    #       indices = indices.write(id, index)
+    #       updates = updates.write(id, 1)
+    #       costargmin_ind += 1
+    #   matching_matrix = tf.tensor_scatter_nd_update(matching_matrix, indices.stack(), tf.cast( updates.stack(), dtype = tf.uint8 ))
+
+    fg_mask_inboxes = tf.math.reduce_sum(matching_matrix, axis=0) > 0
+    num_fg = tf.math.reduce_sum( tf.cast(fg_mask_inboxes, tf.float32) )
+    fg_mask = tf.boolean_mask(fg_mask_inboxes, fg_mask)
+    matched_gt_inds = tf.math.argmax(tf.boolean_mask(matching_matrix, fg_mask_inboxes, axis=1))
+    gt_matched_classes = tf.gather(gt_classes, matched_gt_inds)
+    pred_ious_this_matching_temp1 = tf.math.multiply(tf.cast(matching_matrix, tf.float32), pair_wise_ious)
+    pred_ious_this_matching_temp2 = tf.math.reduce_sum(pred_ious_this_matching_temp1, axis=0) 
+    pred_ious_this_matching = tf.boolean_mask(pred_ious_this_matching_temp2, fg_mask_inboxes)
+
+    return num_fg, gt_matched_classes, pred_ious_this_matching, matched_gt_inds
