a060ad71ed3d57947a8c61fd2d7bfdf86cfc67c7
sklee1216 <54343936+sklee1216@users.noreply.github.com>
2022-04-22 04:09:37 -0400

    Update visualize_mesh.py

--- official/vision/beta/projects/mesh_rcnn/ops/visualize_mesh.py
+++ official/vision/beta/projects/mesh_rcnn/ops/visualize_mesh.py
@@ -18,17 +18,39 @@ def create_voxels(grid_dims, batch_size, occupancy_locs):
     return voxels
 
 
-def visualize_mesh(verts, faces, verts_mask, faces_mask,smoothing = True):
+def visualize_mesh(verts, faces, verts_mask, faces_mask, smoothing = True):
+
+    """Visualize meshes using Open3D
+
+    Args:
+        verts: A "Tensor" of shape of [V,3], where V is the number of vertices
+        faces: A "Tensor" of shape of [F,3], where each face is composed of 3 integers,
+        which map to the points in the vertex tensor
+        verts_mask: A "Tensor" of shape of [V,1], which consists of 0 or 1 to determine
+        which vertices are valid
+        face_mask: A "Tensor" of shape of [F,1], which consists of 0 or 1 to determine
+        which faces are valid
+        smoothing: A 'bool', whether to make meshes smooth
+    """
+
+
     triangle_mesh = o3d.geometry.TriangleMesh()
     verts_numpy = verts.numpy()
     fm = faces_mask.numpy() == 1
     faces_numpy = faces.numpy()[fm]
 
+    # Convert numpy array to Open3D format
     triangle_mesh.vertices = o3d.utility.Vector3dVector(verts_numpy)
     triangle_mesh.triangles = o3d.utility.Vector3iVector(faces_numpy)
 
-    if smoothing: mesh_out = triangle_mesh.filter_smooth_simple(number_of_iterations = 5)
+    # If smoothing is True, smooth the meshes
+    if smoothing:
+        mesh_out = triangle_mesh.filter_smooth_simple(number_of_iterations = 5)
+
+    # Compute triangle normals
     mesh_out.compute_vertex_normals()
+
+    # Draw the meshes and visualize
     o3d.visualization.draw_geometries([mesh_out])
 
 
