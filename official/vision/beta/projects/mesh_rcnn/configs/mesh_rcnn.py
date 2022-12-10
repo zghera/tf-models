b9022cd5d71686a56627860db6de5aa1b9a0c0e8
Nidarshan Siddegowda <34317175+nidarshans@users.noreply.github.com>
2022-12-10 17:38:55 -0500

    Update mesh_rcnn.py

--- official/vision/beta/projects/mesh_rcnn/configs/mesh_rcnn.py
+++ official/vision/beta/projects/mesh_rcnn/configs/mesh_rcnn.py
@@ -87,7 +87,7 @@ def mesh_training() -> cfg.ExperimentConfig:
               },
               'optimizer': {
                   'type': 'adam',
-                  'sgd_torch': {
+                  'adam': {
                       'learning_rate' : 0.001, 
                       'beta_1' : 0.9, 
                       'beta_2' : 0.999, 
